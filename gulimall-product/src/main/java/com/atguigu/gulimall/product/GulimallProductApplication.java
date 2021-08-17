7cba66a3981bfeb2efafc39120dbcdaa0a5d9743
CyS <1440870444@qq.com>
2021-08-17 21:26:12 +0800

    2021.08.17 整合mybatis-plus

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/GulimallProductApplication.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/GulimallProductApplication.java
@@ -1,8 +1,24 @@
 package com.atguigu.gulimall.product;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 1. 整个Mybatis-plus依赖
+ * 1) 导入依赖
+ * <dependency>
+ * <groupId>com.baomidou</groupId>
+ * <artifactId>mybatis-plus-boot-starter</artifactId>
+ * <version>3.2.0</version>
+ * </dependency>
+ * 2) 配置
+ * - 导入mysql驱动的依赖，放在gulimall-common模块了
+ * - 在application.yml配置mysql数据源相关信息
+ * - 配置mybatis-plus: 配置@MapperScan扫描的包路径
+ * - 配置mybatis-plus: 配置classpath扫描的xml文件路径
+ */
+@MapperScan("com.atguigu.gulimall.product.dao")
 @SpringBootApplication
 public class GulimallProductApplication {
 
--- gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
@@ -1,7 +1,9 @@
 package com.atguigu.gulimall.product;
 
+import com.atguigu.gulimall.product.service.BrandService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +11,24 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class GulimallProductApplicationTests {
 
+    @Autowired
+    BrandService brandService;
+
     @Test
     public void contextLoads() {
+//        BrandEntity brandEntity = new BrandEntity();
+//        brandEntity.setBrandId(1L);
+//        brandEntity.setDescript("通信");
+//        brandService.updateById(brandEntity);
+
+//        brandEntity.setName("中兴");
+//        brandService.save(brandEntity);
+//        System.out.println("保存成功...");
+
+//        List<BrandEntity> list = brandService.list(new QueryWrapper<BrandEntity>().eq("brand_id", 1L));
+//        for (BrandEntity entity : list) {
+//            System.out.println(entity);
+//        }
     }
 
 }
