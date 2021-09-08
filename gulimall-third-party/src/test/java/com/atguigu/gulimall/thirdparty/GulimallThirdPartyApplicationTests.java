d3a30cb94c737905b1e00e4df0ab20386fd2fe17
CyS <1440870444@qq.com>
2021-09-08 23:54:12 +0800

    2021.09.04 对象存储

--- gulimall-third-party/src/test/java/com/atguigu/gulimall/thirdparty/GulimallThirdPartyApplicationTests.java
+++ gulimall-third-party/src/test/java/com/atguigu/gulimall/thirdparty/GulimallThirdPartyApplicationTests.java
@@ -36,7 +36,7 @@ public class GulimallThirdPartyApplicationTests {
 		// 填写本地文件的完整路径。如果未指定本地路径，则默认从示例程序所属项目对应本地路径中上传文件流。
 		InputStream inputStream = new FileInputStream("C:\\Users\\Administrator\\Pictures\\许昕2.jpg");
 		// 依次填写Bucket名称（例如examplebucket）和Object完整路径（例如exampledir/exampleobject.txt）。Object完整路径中不能包含Bucket名称。
-		ossClient.putObject("cys-gulimall", "haha.jpg", inputStream);
+		ossClient.putObject("cys-gulimall", "hengheng.jpg", inputStream);
 		System.out.println("上传完成...");
 		// 关闭OSSClient。
 		ossClient.shutdown();
